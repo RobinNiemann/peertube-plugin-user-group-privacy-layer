b81b9663bead8eee585d3227972113a422a6ad65
Robin Niemann <robin@rn-software-services.de>
2025-04-05 13:27:17 +0200

    Beispiel Backend und Frontend Route

--- client/common-client-plugin.ts
+++ client/common-client-plugin.ts
@@ -4,9 +4,24 @@ NB: if you need some types like `video`, `playlist`, ..., you can import them li
 import type { Video } from '@peertube/peertube-types'
 */
 
-async function register ({ peertubeHelpers }: RegisterClientOptions): Promise<void> {
-  const message = await peertubeHelpers.translate('Hello world')
-  console.log(message)
+async function register ({ peertubeHelpers, registerClientRoute, registerHook }: RegisterClientOptions): Promise<void> {
+  
+  registerClientRoute({
+    route: '/my-account/user-groups',
+    onMount: async ({ rootEl }) => {
+      const html = await peertubeHelpers.translate('Benutzergruppen')
+      rootEl.innerHTML = `
+        <div class="margin-content">
+          <h1>Benutzergruppen</h1>
+          <div class="user-groups-container">
+            ${html}
+          </div>
+        </div>
+      `
+    }
+  })
+
+
 }
 
 export {
