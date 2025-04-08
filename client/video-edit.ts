99cee75188ea98445f3535c97fd2e66588d01fc5
Robin Niemann <robin@rn-software-services.de>
2025-04-08 12:11:18 +0200

    Neues Feld in Video Einstellungen

---
+++ client/video-edit.ts
@@ -0,0 +1,39 @@
+import type { RegisterClientOptions } from '@peertube/peertube-types/client'
+import { RegisterClientVideoFieldOptions } from '@peertube/peertube-types'
+
+async function register ({
+  registerVideoField,
+  peertubeHelpers
+}: RegisterClientOptions): Promise<void> {
+
+  const { translate } = peertubeHelpers
+
+  const types: Array<RegisterClientVideoFieldOptions['type']> =
+    ['update', 'upload', 'import-url', 'import-torrent', 'go-live']
+
+  for (const type of types) {
+    registerVideoField({
+      name: "user-groups",
+      label: await translate('User groups'),
+      type: 'select',
+      options: [
+        {
+          value: 'Dummy Group 1',
+          label: await translate('Dummy Group 1')
+        },
+        {
+          value: 'Dummy Group 2',
+          label: await translate('Dummy Group 2')
+        }
+      ],
+      default: 'Dummy Group 2'
+    }, {
+      type,
+      tab: 'main'
+    })
+  }
+}
+
+export {
+  register
+}
